5e7b46356492ca5c123b55137963a0325f36286b
Benjamin Russig <benjamin.russig@tu-dresden.de>
2022-11-01 17:57:12 +0100

    Eliminate most compiler warnings

--- CMake/Modules/CG1Helper.cmake
+++ CMake/Modules/CG1Helper.cmake
@@ -1,15 +1,16 @@
+
 FUNCTION(PREPEND var prefix)
-   SET(listVar "")
-   FOREACH(f ${ARGN})
-      LIST(APPEND listVar "${prefix}/${f}")
-   ENDFOREACH(f)
-   SET(${var} "${listVar}" PARENT_SCOPE)
+	SET(listVar "")
+	FOREACH(f ${ARGN})
+		LIST(APPEND listVar "${prefix}/${f}")
+	ENDFOREACH(f)
+	SET(${var} "${listVar}" PARENT_SCOPE)
 ENDFUNCTION(PREPEND)
 
 FUNCTION(JOIN VALUES GLUE OUTPUT)
-  string (REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${VALUES}")
-  string (REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
-  set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+	string (REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${VALUES}")
+	string (REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
+	set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
 ENDFUNCTION()
 
 FUNCTION(ProcessGLSLFiles GLSL_FILES_VAR)
@@ -27,57 +28,103 @@ FUNCTION(ProcessGLSLFiles GLSL_FILES_VAR)
 		-P "${NANOGUI_DIR}/resources/bin2c.cmake")
 
 	add_custom_command(
-	  OUTPUT glsl.cpp glsl.h
-	  COMMAND ${CMAKE_COMMAND} ARGS ${bin2c_cmdline}
-	  DEPENDS ${GLSL_FILES}
-	  COMMENT "Running bin2c"
-	  PRE_BUILD VERBATIM)
+		OUTPUT glsl.cpp glsl.h
+		COMMAND ${CMAKE_COMMAND} ARGS ${bin2c_cmdline}
+		DEPENDS ${GLSL_FILES}
+		COMMENT "Running bin2c"
+		PRE_BUILD VERBATIM)
 ENDFUNCTION()
 
 FUNCTION(SetupBuildEnvironment)
 	# Enable folders for projects in Visual Studio
-	if (CMAKE_GENERATOR MATCHES "Visual Studio")
-	  set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+	if(CMAKE_GENERATOR MATCHES "Visual Studio")
+		set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 	endif()
 
-	# Sanitize build environment for static build with C++11
-	if (MSVC)
-	  add_definitions (/D "_CRT_SECURE_NO_WARNINGS")
-	  add_definitions (/D "__TBB_NO_IMPLICIT_LINKAGE")
+	# Sanitize build environment for static build with C++14
+	set(MICROSOFT_CRT_DEFINES "_CRT_SECURE_NO_WARNINGS=1;__TBB_NO_IMPLICIT_LINKAGE=1;NOMINMAX")
+	if(MSVC)
+		# Get more familiar MSVC standard library behaviour
+		add_compile_definitions(${MICROSOFT_CRT_DEFINES})
 
-	  add_definitions (-DNOMINMAX )
-	  add_definitions(/D_USE_MATH_DEFINES)
+		# Parallel build on MSVC (all targets)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
 
-	  # Parallel build on MSVC (all targets)
-	  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+		if(NOT CMAKE_SIZEOF_VOID_P EQUAL 8)
+			# Enable use of SIMD for 32bit builds...
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:SSE2")
 
-	  if (NOT CMAKE_SIZEOF_VOID_P EQUAL 8)
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:SSE2")
+			# ... but disable Eigen vectorization as it causes segfaults on 32bit MSVC builds
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DEIGEN_DONT_ALIGN")
+	 	 endif()
 
-		# Disable Eigen vectorization for Windows 32 bit builds (issues with unaligned access segfaults)
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DEIGEN_DONT_ALIGN")
-	  endif()
-
-	  # Static build
-	  set(CompilerFlags
-			CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-			CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
-			CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
-			CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
-	  foreach(CompilerFlag ${CompilerFlags})
-		string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
-	  endforeach()
+		# Static build
+		set(CompilerFlags
+				CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+				CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
+				CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+				CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+		foreach(CompilerFlag ${CompilerFlags})
+			string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+		endforeach()
+	elseif(WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		add_compile_definitions(${MICROSOFT_CRT_DEFINES})
 	elseif(APPLE)
-	  # Try to auto-detect a suitable SDK
-	  execute_process(COMMAND bash -c "xcodebuild -version -sdk | grep MacOSX | grep Path | head -n 1 | cut -f 2 -d ' '" OUTPUT_VARIABLE CMAKE_OSX_SYSROOT)
-	  string(REGEX REPLACE "(\r?\n)+$" "" CMAKE_OSX_SYSROOT "${CMAKE_OSX_SYSROOT}")
-	  string(REGEX REPLACE "^.*X([0-9.]*).sdk$" "\\1" CMAKE_OSX_DEPLOYMENT_TARGET "${CMAKE_OSX_SYSROOT}")
+		# Try to auto-detect a suitable SDK
+		execute_process(COMMAND bash -c "xcodebuild -version -sdk | grep MacOSX | grep Path | head -n 1 | cut -f 2 -d ' '" OUTPUT_VARIABLE CMAKE_OSX_SYSROOT)
+		string(REGEX REPLACE "(\r?\n)+$" "" CMAKE_OSX_SYSROOT "${CMAKE_OSX_SYSROOT}")
+		string(REGEX REPLACE "^.*X([0-9.]*).sdk$" "\\1" CMAKE_OSX_DEPLOYMENT_TARGET "${CMAKE_OSX_SYSROOT}")
 	endif()
 
-	if(CMAKE_COMPILER_IS_GNUCXX OR CLANG) 
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations -Wno-unused-result -Wno-misleading-indentation")
-	endif() 
-
+	# Export settings to global scope so they're used as basis for later per-target configurations
 	set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} PARENT_SCOPE) 
-	set (CMAKE_CXX_STANDARD 14 PARENT_SCOPE)
+	set(CMAKE_CXX_STANDARD 14 PARENT_SCOPE)
+ENDFUNCTION()
+
+FUNCTION(FinishBuildEnvironment)
+	# Inject compile options into 3rd party targets to try and get rid of as many warnings as possible
+	#- NanoGUI
+	if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+		# Common to both GCC and Clang
+		if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "GNU")
+			target_compile_options(nanogui-obj PRIVATE -Wno-unused-command-line-argument -Wno-unknown-pragmas -Wno-deprecated-declarations -Wno-unused-result -Wno-misleading-indentation -Wno-implicit-fallthrough)
+		endif()
+
+		# Special options depending on whether it's GCC or Clang
+		if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+			# Warnings generated all over the code base
+			target_compile_options(nanogui-obj PRIVATE -Wno-cast-function-type)
+
+			# Warnings we need to remove surgically from certain files in order to prevent other warnings from appearing
+			set_source_files_properties("${NANOGUI_DIR}/src/button.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/checkbox.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/colorpicker.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/colorwheel.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/combobox.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/common.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/glcanvas.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/glutil.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/graph.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/imagepanel.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/imageview.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/label.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/layout.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/messagedialog" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/popup.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/popupbutton.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/progressbar.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/screen.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/serializer.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/slider.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/stackedwidget.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/tabheader.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/tabwidget.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/textbox.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/vscrollpanel.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/window.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+			set_source_files_properties("${NANOGUI_DIR}/src/widget.cpp" TARGET_DIRECTORY nanogui-obj PROPERTIES COMPILE_FLAGS -Wno-deprecated-copy)
+		elseif(CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "GNU")
+			target_compile_options(nanogui-obj PRIVATE -Wno-bad-function-cast)
+		endif()
+	endif()
 ENDFUNCTION()
